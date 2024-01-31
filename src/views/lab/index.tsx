cce86246812002bb346afbe0147942586e6fe8cb
명성[디지캡] <smyeong@digicaps.com>
2024-01-31 10:08:30 +0900

    TTS 로컬 연동 테스트케이스 추가

--- src/views/lab/index.tsx
+++ src/views/lab/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button';
 import StreamNumbers from './components/StreamNumber';
+import AudioPlayer from '@/views/test'
 
 const Lab = () => {
   return (
     <div className="flex flex-col h-screen p-4">
       <StreamNumbers />
+      <AudioPlayer />
     </div>
   );
 };
